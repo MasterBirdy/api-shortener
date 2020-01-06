c1164c7b6d7dc80295d6b600afecffd0074beda3
Matthew Ito <mito81@gmail.com>
2020-01-06 01:49:35 -0800

    added validation

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ class easyHTTP {
         const response = await fetch(url);
         const resData = await response.json();
         return resData;
-   }
+    }
 
     async post(theurl, data) {
-        const response = await fetch (theurl, {
+        const response = await fetch(theurl, {
             method: "POST",
             headers: {
                 "Content-type": "application/json"
             },
-            body: JSON.stringify({url: data})
+            body: JSON.stringify({ url: data })
         })
         const resData = await response.json();
         return resData;
@@ -45,14 +45,33 @@ class UI {
         shortenDiv.innerHTML = "";
     }
 
-    changeCopyButton(button)
-    {
+    changeCopyButton(button) {
         button.classList.add("black-copy");
         button.textContent = "Copied!";
         setTimeout(() => {
             button.classList.remove("black-copy");
             button.textContent = "Copy";
         }, 3000)
+    }
+
+    showAlert(message, alertClass) {
+        const shortenSection = document.querySelector(".shorten-section");
+        const shortestSection = document.querySelector(".shortest-section");
+        const alert = document.createElement("p");
+        alert.textContent = message;
+        alert.className = alertClass;
+        shortenSection.classList.add("alert-div");
+        shortenInput.classList.add(`${alertClass}-input`);
+        shortestSection.insertAdjacentElement("afterend", alert);
+        setTimeout(() => {
+            shortenInput.classList.remove(`${alertClass}-input`);
+            shortenSection.classList.remove("alert-div");
+            alert.remove();
+        }, 3000)
+    }
+
+    clearInput() {
+        shortenInput.value = "";
     }
 }
 
@@ -85,25 +104,28 @@ const easyhttp = new easyHTTP();
 const ui = new UI();
 const ls = new LocalStorage();
 const shortenBtn = document.querySelector(".shorten-btn")
-  .addEventListener("click", (e) => {
-      easyhttp.post(`https://rel.ink/api/links/?url=${shortenInput.value}`, shortenInput.value)
-        .then(data => {
-            ls.addURL(data.url, data.hashid);
-            ui.clearURLs();
-            ls.loadURLs().forEach(url => {
-                ui.createURLPost(url.url, url.hashurl)
-            });
-        })
-        .catch(err => console.log(err));
-})
+    .addEventListener("click", (e) => {
+        easyhttp.post(`https://rel.ink/api/links/?url=${shortenInput.value}`, shortenInput.value)
+            .then(data => {
+                if (validate(data.url)) {
+                    ls.addURL(data.url, data.hashid);
+                    ui.clearURLs();
+                    ls.loadURLs().forEach(url => {
+                        ui.createURLPost(url.url, url.hashurl)
+                    });
+                    ui.clearInput();
+                }
+            })
+            .catch(err => console.log(err));
+    })
 
 shortenDiv.addEventListener("click", (e) => {
-    if (e.target.classList.contains("hashButton")){
+    if (e.target.classList.contains("hashButton")) {
         navigator.clipboard.writeText(e.target.previousElementSibling.textContent)
-          .then(() =>  {
-              ui.changeCopyButton(e.target);
-        })
-          .catch(err => console.log(err));
+            .then(() => {
+                ui.changeCopyButton(e.target);
+            })
+            .catch(err => console.log(err));
     }
 });
 
@@ -112,3 +134,17 @@ document.addEventListener("DOMContentLoaded", () => {
         ui.createURLPost(url.url, url.hashurl)
     });
 });
+
+function validate(url) {
+    const regex = /^https?:\/\/(www.)?\w[A-Za-z0-9-]+\.\w{2,}$/i;
+    if (url === "") {
+        ui.showAlert("Please enter a URL!", "alert");
+        return false;
+    } else if (!regex.test(url)) {
+        ui.showAlert("Please enter a valid URL with http:// in front!", "alert");
+        return false;
+    }
+    ui.showAlert("Link added!", "success");
+    return true;
+
+}
